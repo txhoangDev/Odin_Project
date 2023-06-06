698f726b622c3f4a88c23586987cf50ccbbffe5f
Thuong Hoang <thuonghoang@Thuongs-Air.attlocal.net>
2023-06-05 19:57:07 -0500

    finished css and html for ui

--- rock-paper-scissor/rockPaperScissor.js
+++ rock-paper-scissor/rockPaperScissor.js
@@ -1,40 +1,66 @@
+// adding event listeners
+const btns = document.querySelectorAll('#choice');
+btns.forEach(btn => btn.addEventListener('click', getWinner))
+
+const div = document.querySelector('#result');
+div.style = 'justify-content: center; align-items: center;';
+const text = document.querySelector('#text');
+const score = document.querySelector('#score'); 
+
 const turn = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return turn[Math.floor(Math.random() * (3))];
 }
 
-function getWinner(computerChoice, playerChoice) {
-    if(playerChoice == computerChoice) {
-        return 'DRAW!';
-    } else if (playerChoice == 'rock') {
-        switch (computerChoice) {
-            case 'paper':
-                return 'You lose! Paper beats Rock';
-            case 'scissors':
-                return 'You Win!';
-        }
-    } else if (playerChoice == 'paper') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'You Win!';
-            case 'scissors':
-                return 'You lose! Scissors beats Paper';
-        }
-    } else if (playerChoice == 'scissors') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'You lose! Rock beats Scissors';
-            case 'paper':
-                return 'You Win!';
-        }
-    }
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
 }
 
-function game() {
-    for(i = 0; i < 5; i ++) {
-        let player = prompt("Choose Rock, Paper, or Scissors.");
-        let computer = getComputerChoice();
-        console.log(getWinner(player.toLowerCase(), computer));
+function getWinner(e) {
+    const player = e.target.textContent.toLowerCase();
+    const computer = getComputerChoice();
+    if(player === computer) {  
+        text.textContent = 'Draw';
+    } else if (player === 'rock') {
+        switch (computer) {
+            case 'paper':
+                text.textContent = 'You lose! Paper beats Rock';
+                computerScore += 1;
+                break;
+            case 'scissors':
+                text.textContent = 'You Win!';
+                playerScore += 1;
+                break;
+        }
+    } else if (player === 'paper') {
+        switch (computer) {
+            case 'rock':
+                text.textContent = 'You Win!';
+                playerScore += 1;
+                break;
+            case 'scissors':
+                text.textContent = 'You lose! Scissor beats Paper';
+                computerScore += 1;
+                break;
+        }
+    } else if (player === 'scissor') {
+        switch (computer) {
+            case 'rock':
+                text.textContent = 'You lose! Rock beats Scissor';
+                computerScore += 1;
+                break;
+            case 'paper':
+                text.textContent = 'You Win!';
+                playerScore += 1;
+                break;
+        }
     }
+    if (playerScore === 5 || computerScore === 5) {
+        reset();
+    }
+    score.textContent = `${playerScore} - ${computerScore}`;
 }
