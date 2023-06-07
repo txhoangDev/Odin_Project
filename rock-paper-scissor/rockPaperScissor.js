f9b7a08157e0af1f8e28be09f739ee97240917f2
Thuong Hoang <thuonghoang@Thuongs-Air.attlocal.net>
2023-06-07 16:40:46 -0500

    finished typing effect

--- rock-paper-scissor/rockPaperScissor.js
+++ rock-paper-scissor/rockPaperScissor.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var currentYear = new Date().getFullYear();
+    var copyrightFooter = document.getElementById("footer");
+    copyrightFooter.innerHTML =
+      "Copyright &copy; " + currentYear + " Thuong Hoang";
+  });
+
 // adding event listeners
 const btns = document.querySelectorAll('#choice');
 btns.forEach(btn => btn.addEventListener('click', getWinner))
@@ -5,62 +12,80 @@ btns.forEach(btn => btn.addEventListener('click', getWinner))
 const div = document.querySelector('#result');
 div.style = 'justify-content: center; align-items: center;';
 const text = document.querySelector('#text');
-const score = document.querySelector('#score'); 
-
-const turn = ['rock', 'paper', 'scissors'];
-let playerScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
-    return turn[Math.floor(Math.random() * (3))];
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * (3))];
 }
 
 function reset() {
-    playerScore = 0;
-    computerScore = 0;
+    pscore.textContent = '0';
+    cscore.textContent = '0';   
 }
 
 function getWinner(e) {
-    const player = e.target.textContent.toLowerCase();
+    const pscore = document.querySelector('#pscore');
+    const cscore = document.querySelector('#cscore');
+    const text = document.querySelector('#text');
+    let player = e.target.alt;
+    if (player == null) {
+        player = e.target.querySelector('img').alt;
+    }
     const computer = getComputerChoice();
-    if(player === computer) {  
-        text.textContent = 'Draw';
+    if (player === computer) {
+        typewriterEffect(text, 'Draw!');
     } else if (player === 'rock') {
         switch (computer) {
             case 'paper':
-                text.textContent = 'You lose! Paper beats Rock';
-                computerScore += 1;
+                typewriterEffect(text, 'You lose! Paper beats Rock');
+                cscore.textContent = +cscore.textContent + 1;
                 break;
             case 'scissors':
-                text.textContent = 'You Win!';
-                playerScore += 1;
+                typewriterEffect(text, 'You Win!');
+                pscore.textContent = +pscore.textContent + 1;
                 break;
         }
     } else if (player === 'paper') {
         switch (computer) {
             case 'rock':
-                text.textContent = 'You Win!';
-                playerScore += 1;
+                typewriterEffect(text, 'You Win!');
+                pscore.textContent = +pscore.textContent + 1;
                 break;
             case 'scissors':
-                text.textContent = 'You lose! Scissor beats Paper';
-                computerScore += 1;
+                typewriterEffect(text, 'You lose! Scissor beats Paper');
+                cscore.textContent = +cscore.textContent + 1;
                 break;
         }
     } else if (player === 'scissor') {
         switch (computer) {
             case 'rock':
-                text.textContent = 'You lose! Rock beats Scissor';
-                computerScore += 1;
+                typewriterEffect(text, 'You lose! Rock beats Scissor');
+                cscore.textContent = +cscore.textContent + 1;
                 break;
             case 'paper':
-                text.textContent = 'You Win!';
-                playerScore += 1;
+                typewriterEffect(text, 'You Win!');
+                pscore.textContent = +pscore.textContent + 1;
                 break;
         }
     }
-    if (playerScore === 5 || computerScore === 5) {
+    if (pscore.textContent == 5 || cscore.textContent == 5) {
         reset();
     }
-    score.textContent = `${playerScore} - ${computerScore}`;
 }
+
+// Function to animate the typewriter effect
+function typewriterEffect(element, message) {
+    element.textContent = ''; // Clear the element's current content
+    const characters = message.split(''); // Split the message into individual characters
+
+    let index = 0;
+    const timer = setInterval(() => {
+        element.textContent += characters[index];
+        index++;
+        if (index === characters.length) {
+            clearInterval(timer);
+        }
+    }, 100); // Adjust the typing speed by changing the interval duration (e.g., 100ms)
+
+    element.classList.add('typing'); // Apply the typing animation style
+}
+
